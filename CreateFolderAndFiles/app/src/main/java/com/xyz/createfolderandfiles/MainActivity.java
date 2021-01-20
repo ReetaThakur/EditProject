0084feb28618ab1b3ed581e5a989fb2bc7747202
Lloyd <dcostalloyd90@gmail.com>
2021-01-20 16:15:51 +0530

    Moved all the operations to background thread

--- CreateFolderAndFiles/app/src/main/java/com/xyz/createfolderandfiles/MainActivity.java
+++ CreateFolderAndFiles/app/src/main/java/com/xyz/createfolderandfiles/MainActivity.java
@@ -1,6 +1,7 @@
 package com.xyz.createfolderandfiles;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -8,14 +9,87 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button mBtnSaveLogs;
     private Button mBtnGetLogs;
+
+    /**
+     * Runnable for save the data to the file
+     */
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            int[] a = new int[5];
+            try {
+                a[5] = 10;
+
+            } catch (Exception e) {
+                saveLogsToFile(e.getLocalizedMessage());
+            }
+
+        }
+    };
+
+    /**
+     * Runnable for reading the data from file
+     */
+    private Runnable readRunnable = new Runnable() {
+        @Override
+        public void run() {
+            readFile();
+        }
+    };
+
+    /**
+     * This method is used to save the file to the internal shared storage
+     *
+     * @param message exception message that needs to be saved to the file
+     */
+    private void saveLogsToFile(String message) {
+        //Create a folder
+        try {
+            /*
+            getFilesDir() creates a file in the location data/data/<app-package> -> internal shared storage (user cannot access)
+            getExternalFilesDir(null) creates a file in sdcard/Android/<app-package> -> external storage (user can access)
+            Environment.getExternalStorageDirectory() creates a file in external SD card
+             */
+            File directory = new File(getFilesDir() + File.separator + "logsFolder");
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
+
+            // Create file inside the directory
+            File file = new File(directory, "logs.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+            OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream);
+            writer.append(message + "\n");
+            writer.close();
+            updateUI();
+        } catch (Exception e) {
+            Log.d("Lloyd", e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Once the data is saved in the file then display a Toast in the UI thread.
+     */
+    private void updateUI() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "File successfully created", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,50 +104,62 @@ public class MainActivity extends AppCompatActivity {
         mBtnSaveLogs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                createExceptionAndSaveLogs();
+                startBackgroundThread();
+            }
+        });
+
+        mBtnGetLogs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread thread = new Thread(readRunnable);
+                thread.start();
             }
         });
     }
 
-    private void createExceptionAndSaveLogs() {
-        int[] a = new int[5];
-
-        try {
-            a[5] = 4;
-        } catch (Exception e) {
-            saveLogsToFile(e.getLocalizedMessage());
-        }
-
+    private void startBackgroundThread() {
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 
-    private void saveLogsToFile(String message) {
-        File directory = new File(this.getFilesDir() + File.separator + "ExceptionFolder");
-
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
-
-        File newFile = new File(directory, "logs.txt");
-
-        if (!newFile.exists()) {
-            try {
-                newFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
+    /**
+     * This method is used to read the file in the background thread
+     */
+    private void readFile() {
         try {
-            FileOutputStream fOut = new FileOutputStream(newFile,true);
-            OutputStreamWriter outputWriter = new OutputStreamWriter(fOut);
-            outputWriter.append(message + "\n");
-            outputWriter.close();
+            File directory = new File(getFilesDir() + File.separator + "logsFolder");
 
-            //display file saved message
-            Toast.makeText(getBaseContext(), "File saved successfully!",
-                    Toast.LENGTH_SHORT).show();
+            // Create file inside the directory
+            File file = new File(directory, "logs.txt");
+            FileInputStream inputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+            int data = inputStreamReader.read();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while (data != -1) {
+                char ch = (char) data;
+                stringBuilder.append(ch);
+                data = inputStreamReader.read();
+            }
+            UpdateReadData(stringBuilder);
         } catch (Exception e) {
-            e.printStackTrace();
+
         }
+    }
+
+    /**
+     * Once the data is read from the file display a Toast in the UI thread
+     *
+     * @param builder data read from the file
+     */
+    private void UpdateReadData(StringBuilder builder) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "Data read successfully", Toast.LENGTH_SHORT).show();
+                Log.d("Lloyd", builder.toString());
+            }
+        });
     }
 }
